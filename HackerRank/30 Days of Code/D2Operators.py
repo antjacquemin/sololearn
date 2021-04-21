11ab7e6f89f44fb5f9a6c7ed0e9418640a9e1f04
Anthony JACQUEMIN <anthonyjacquemin@hotmail.fr>
2021-04-21 08:24:19 +0200

    30 days of code P1

---
+++ HackerRank/30 Days of Code/D2Operators.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    print(round(meal_cost * (100 + tip_percent + tax_percent) / 100))
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
