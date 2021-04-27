f2e17100b687e5bb10eb17350860bdda90000c92
Anthony JACQUEMIN <anthonyjacquemin@hotmail.fr>
2021-04-28 01:50:08 +0200

    C++ introduction 1-3

---
+++ HackerRank/C++/3BasicDataTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+    printf("%d\n%ld\n%c\n%f\n%lf", i, l, c, f, d);
+    return 0;
+}
