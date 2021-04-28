680e15fe78695318be751bc0795cefbf6a9569e1
Anthony JACQUEMIN <anthonyjacquemin@hotmail.fr>
2021-04-28 03:00:59 +0200

    SQL Basic select

---
+++ HackerRank/SQL/1BasicSelect.sql
@@ -0,0 +1,61 @@
+-- 1 Revising the Select Query I
+SELECT * FROM CITY WHERE COUNTRYCODE = "USA" AND POPULATION > 100000;
+
+-- 2 Revising the Select Query 2
+SELECT NAME FROM CITY WHERE COUNTRYCODE = "USA" AND POPULATION > 120000;
+
+-- 3 Select All
+SELECT * FROM CITY;
+
+-- 4 Select By ID
+SELECT * FROM CITY WHERE ID = 1661;
+
+-- 5 Japanese Cities' Attributes
+SELECT * FROM CITY WHERE COUNTRYCODE = "JPN";
+
+-- 6 Japanese Cities' Names
+SELECT NAME FROM CITY WHERE COUNTRYCODE = "JPN";
+
+-- 7 Weather Observation Station 1
+SELECT CITY, STATE FROM STATION;
+
+-- 8 Weather Observation Station 3 (sic)
+SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0;
+
+-- 9 Weather Observation Station 4
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
+
+-- 10 Weather Observation Station 5
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY), CITY LIMIT 1;
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY LIMIT 1;
+
+-- 11 Weather Observation Station 6
+SELECT DISTINCT CITY FROM STATION WHERE LOWER(CITY) REGEXP "^[aeiou]"; 
+
+-- 12 Weather Observation Station 7
+SELECT DISTINCT CITY FROM STATION WHERE LOWER(CITY) REGEXP "[aeiou]$";
+
+-- 13 Weather Observation Station 8
+SELECT DISTINCT CITY FROM STATION WHERE LOWER(CITY) REGEXP "^[aeiou].*[aeiou]$";
+
+-- 14 Weather Observation Station 9
+SELECT DISTINCT CITY FROM STATION WHERE LOWER(CITY) NOT REGEXP "^[aeiou]"; 
+#SELECT DISTINCT CITY FROM STATION WHERE LOWER(CITY) REGEXP '^[^aeiou]';
+
+-- 15 Weather Observation Station 10
+SELECT DISTINCT CITY FROM STATION WHERE LOWER(CITY) REGEXP "[^aeiou]$";
+
+-- 16 Weather Observation Station 11
+SELECT DISTINCT CITY FROM STATION WHERE LOWER(CITY) REGEXP "^[^aeiou]|[^aeiou]$";
+
+-- 17 Weather Observation Station 12
+SELECT DISTINCT CITY FROM STATION WHERE LOWER(CITY) REGEXP "^[^aeiou].*[^aeiou]$";
+
+-- 18 Higher Than 75 Marks
+SELECT Name FROM STUDENTS WHERE Marks > 75 ORDER BY RIGHT(Name, 3), ID;
+
+-- 19 Employee Names
+SELECT name FROM Employee ORDER BY name;
+
+-- 20 Employee Salaries
+SELECT name FROM Employee WHERE salary > 2000 AND months < 10 ORDER BY employee_id;
