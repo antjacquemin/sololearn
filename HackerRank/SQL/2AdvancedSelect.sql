f421bc9fcc51ba19a82586adb1ea1f2e3bbd0cd4
Anthony JACQUEMIN <anthonyjacquemin@hotmail.fr>
2021-04-28 13:21:46 +0200

    SQL Advanced select 4-5

--- HackerRank/SQL/2AdvancedSelect.sql
+++ HackerRank/SQL/2AdvancedSelect.sql
@@ -6,7 +6,7 @@ SELECT CASE
         WHEN A = B OR B = C OR A = C THEN "Isosceles"
         ELSE "Scalene"
     END
-FROM TRIANGLES;
+    FROM TRIANGLES;
 
 -- 2 The PADS
 SELECT CONCAT(Name, "(", LEFT(Occupation, 1) ,")") FROM OCCUPATIONS ORDER BY NAME;
@@ -15,5 +15,16 @@ SELECT "There are a total of", COUNT(Occupation) AS total, CONCAT(LOWER(Occupati
 -- 3 Occupations
 
 -- 4 Binary Tree Nodes
+SELECT N, CASE
+        WHEN P IS NULL THEN "Root"
+        WHEN EXISTS(SELECT P FROM BST WHERE BParent.N = P) THEN "Inner"
+        ELSE "Leaf"
+    END
+    FROM BST AS BParent ORDER BY N;
 
 -- 5 New Companies
+SELECT DISTINCT c.company_code, founder, COUNT(DISTINCT lead_manager_code), COUNT(DISTINCT senior_manager_code), COUNT(DISTINCT manager_code), COUNT(DISTINCT employee_code) 
+    FROM Company c
+    JOIN Employee e ON c.company_code = e.company_code
+    GROUP BY c.company_code, founder
+    ORDER BY c.company_code;
