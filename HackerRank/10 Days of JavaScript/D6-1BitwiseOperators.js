1d8168f979a045469f7e0f12b6a4006531a1da75
Anthony JACQUEMIN <anthonyjacquemin@hotmail.fr>
2021-05-08 03:02:50 +0200

    10 days of JavaScript D6

---
+++ HackerRank/10 Days of JavaScript/D6-1BitwiseOperators.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getMaxLessThanK(n, k) {
+    let max = 0;
+    for (let a=1; a<n; a++) {
+        for (let b=a+1; b<n+1; b++) {
+            if ((a&b) < k && (a&b) > max) {
+                max = a&b;
+            }
+        }
+    }
+    return max;
+}
+
+function main() {
+    const q = +(readLine());
+    
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+        
+        console.log(getMaxLessThanK(n, k));
+    }
+}
